b13df2736a3e194487e9ee96daa26502a1a05a21
Derek O'Neil <DerekO.Neil@yahoo.com>
2018-08-21 11:27:13 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,20 +1,34 @@
 
 var cards = ["queen", "queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+var checkForMatch = function (){
 
-if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		}else {
+			alert("Sorry, try again.");
+		}
+	
+};
 
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}else {
-		alert("Sorry, try again.");
+var flipCard = function (cardId){
+
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2){
+		checkForMatch();
 	}
-}
+	
+	
+	console.log("User flipped " + cards[cardId]);
+	
 
-console.log("User flipped queen ");
-console.log("User flipped king ");
+
+};
+
+
+flipCard(0);
+flipCard(2);
+
+
