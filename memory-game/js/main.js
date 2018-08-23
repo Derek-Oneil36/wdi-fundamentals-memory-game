dec5a7f43323ca64e0b43dd8296548d1ebe203c4
Derek O'Neil <DerekO.Neil@yahoo.com>
2018-08-22 20:19:52 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -23,6 +23,17 @@ var cards = [{
 
 var cardsInPlay = [];
 
+var createBoard = function (){
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+		
+	}
+}
+
 var checkForMatch = function (){
 
 		if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -33,7 +44,13 @@ var checkForMatch = function (){
 	
 };
 
-var flipCard = function (cardId){
+var flipCard = function (){
+
+
+
+	var cardId = this.getAttribute('data-id');
+
+	this.setAttribute('src', cards[cardId].cardImage)
 
 	cardsInPlay.push(cards[cardId].rank);
 
@@ -41,7 +58,9 @@ var flipCard = function (cardId){
 		checkForMatch();
 	}
 	
+
 	
+
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
@@ -49,7 +68,6 @@ var flipCard = function (cardId){
 };
 
 
-flipCard(0);
-flipCard(2);
+createBoard();
 
 
